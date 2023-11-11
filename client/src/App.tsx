9473794e16373d44e762cd64dc470fbae9e09c56
emmacherrin <emmacherrin@gmail.com>
2023-11-11 13:39:13 -0500

    Converting frontend to TS
    For compatibility with Fluent 2

---
+++ client/src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+import {
+  FluentProvider,
+  webLightTheme,
+  Button
+} from "@fluentui/react-components";
+
+interface AstronautData {
+  heartrate: number;
+  // Add more properties if needed
+}
+
+function App() {
+  const [astronaut, setAstronaut] = useState<string | undefined>(undefined);
+
+  function getAstronaut() {
+    if (astronaut) {
+      axios.get<AstronautData>(`/api/getAstronaut/${astronaut}`).then((res) => {
+        alert(`This astronaut's heartrate is ${res.data.heartrate}`);
+      });
+    }
+  }
+
+  return (
+    <FluentProvider theme={webLightTheme}>
+      <Button appearance="primary">Hello Fluent UI React</Button>
+    </FluentProvider>
+
+    /* UNCOMMENT THIS SECTION IF YOU NEED THE INPUT AND BUTTON PART
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <input onChange={(e) => setAstronaut(e.target.value)} />
+          <button onClick={getAstronaut}>Get Astronaut</button>
+        </header>
+      </div>
+    */
+  );
+}
+
+export default App;
