898495f088fe3971dbe696e95345ba8de51a6eff
emmacherrin <emmacherrin@gmail.com>
2023-11-11 11:20:58 -0500

    New File Structure Hierarchy Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import logo from './assets/logo.png';
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
+import {
+	FluentProvider,
+	webLightTheme,
+	Button
+} from "@fluentui/react-components";
 
 function App() {
 	// text = state variable called text with whatever value
@@ -23,13 +28,23 @@ function App() {
 	}
 
 	return (
-		<div className="App">
+		<FluentProvider theme={webLightTheme}>
+              <Button appearance="primary">Hello Fluent UI React</Button>
+        </FluentProvider>
+
+
+
+		/** EXAMPLE FROM SAIF
+		 * 
+		 * <div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<input onChange={(e) => setAstronaut(e.target.value)} />
 				<button onClick={getAstronaut}>Get Astronaut</button>
 			</header>
 		</div>
+		 */
+		
 	);
 }
 
