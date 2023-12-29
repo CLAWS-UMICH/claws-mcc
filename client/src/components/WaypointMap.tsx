e5e2a6cc50e1dbb1f2d0e372ea2637e583f2aaa5
Oscar Rangel <oscjac3@gmail.com>
2023-12-29 18:26:18 -0400

    Implemented basic functionality in WaypointMap.tsx

--- client/src/components/WaypointMap.tsx
+++ client/src/components/WaypointMap.tsx
@@ -1,133 +1,175 @@
-import React from 'react';
-import { useState, useCallback } from 'react';
-import axios from 'axios';
-import {GoogleMap, Marker, useJsApiLoader, InfoWindow} from '@react-google-maps/api';
+import React, {useState} from 'react';
+import {GoogleMap, InfoWindow, Marker, useJsApiLoader} from '@react-google-maps/api';
 
-const key = "AIzaSyBKoEACDcmaJYjODh0KpkisTk1MPva76s8"; 
+const key = "AIzaSyBKoEACDcmaJYjODh0KpkisTk1MPva76s8";
+
+type MapObject = {
+    location: string,
+    name: string,
+    zoomlevels: number,
+    copyright: string,
+    caption: string
+}
+
+/**
+ * Adds custom map types for each type of Mars imagery.
+ * @param {google.maps.Map} map The map to which to add these map types.
+ */
+function addMarsMapTypes(map: google.maps.Map) {
+    const mapsServer = 'mw1.google.com/mw-planetary/mars/';
+    const maps: { [index: string]: MapObject } = {
+        elevation: {
+            location: mapsServer + 'elevation',
+            name: 'elevation',
+            zoomlevels: 9,
+            copyright: 'NASA / JPL / GSFC / Arizona State University',
+            caption: 'A shaded relief map color-coded by altitude'
+        },
+        visible: {
+            location: mapsServer + 'visible',
+            name: 'visible',
+            zoomlevels: 10,
+            copyright: 'NASA / JPL / MSSS / Arizona State University',
+            caption: 'A mosaic of images from the visible portion of the spectrum'
+        },
+        infrared: {
+            location: mapsServer + 'infrared',
+            name: 'infrared',
+            zoomlevels: 13,
+            copyright: 'NASA / JPL / Arizona State University',
+            caption: 'A mosaic of images from the infrared portion of the spectrum'
+        }
+    };
+
+    map.mapTypes.set('elevation', makeMarsMapType(maps['elevation']));
+    map.mapTypes.set('visible', makeMarsMapType(maps['visible']));
+    map.mapTypes.set('infrared', makeMarsMapType(maps['infrared']));
+
+    map.setMapTypeId('elevation');
+}
+
+/**
+ * Builds a custom map type for Mars based on the provided parameters.
+ * @param {Object} m JSON representing the map type options.
+ * @return {google.maps.ImageMapType} .
+ */
+function makeMarsMapType(m: MapObject): google.maps.ImageMapType {
+    const opts = {
+        baseUrl: 'https://' + m.location + '/',
+        getTileUrl: function (tile: google.maps.Point, zoom: number) {
+            let bound = Math.pow(2, zoom);
+            let x = tile.x;
+            let y = tile.y;
+            // Don't repeat across y-axis (vertically).
+            if (y < 0 || y >= bound) {
+                return null;
+            }
+
+            // Repeat across x-axis.
+            if (x < 0 || x >= bound) {
+                x = (x % bound + bound) % bound;
+            }
+            let qstr = 't';
+            for (let z = 0; z < zoom; z++) {
+                bound = bound / 2;
+                if (y < bound) {
+                    if (x < bound) {
+                        qstr += 'q';
+                    } else {
+                        qstr += 'r';
+                        x -= bound;
+                    }
+                } else {
+                    if (x < bound) {
+                        qstr += 't';
+                        y -= bound;
+                    } else {
+                        qstr += 's';
+                        x -= bound;
+                        y -= bound;
+                    }
+                }
+            }
+            return 'https://' + m.location + '/' + qstr + '.jpg';
+        },
+        tileSize: new google.maps.Size(256, 256),
+        maxZoom: m.zoomlevels - 1,
+        minZoom: 0,
+        name: m.name.charAt(0).toUpperCase() + m.name.substr(1)
+    };
+
+    return new google.maps.ImageMapType(opts);
+}
 
 const containerStyle = {
     width: '400px',
     height: '400px'
- };
-  
+};
+
 const center = {
     lat: -3.745,
     lng: -38.523
 };
 
-const options = { 
-    mapTypeId: 'satellite',
-    disableDefaultUI: true,
+type WaypointMarker = {
+    waypoint_id: number,
+    location: number[], // [lat, lng]
+    type: string,
+    description: string,
+    author: number
 }
 
-// Normalizes the coords that tiles repeat across the x axis (horizontally)
-// like the standard Google map tiles.
-function getNormalizedCoord(coord, zoom) {
-  var y = coord.y;
-  var x = coord.x;
-
-  // tile range in one direction range is dependent on zoom level
-  // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
-  var tileRange = 1 << zoom;
-
-  // don't repeat across y-axis (vertically)
-  if (y < 0 || y >= tileRange) {
-    return null;
-  }
-
-  // repeat across x-axis
-  if (x < 0 || x >= tileRange) {
-    x = (x % tileRange + tileRange) % tileRange;
-  }
-
-  return {x: x, y: y};
+interface WaypointMapProps {
+    waypoints: WaypointMarker[];
 }
 
-function WaypointMap(props) {
-    const { isLoaded } = useJsApiLoader({
-      id: 'google-map-script',
-      googleMapsApiKey: key
+function WaypointMap(props: WaypointMapProps) {
+    const {isLoaded} = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: key
     })
-  
-    const [map, setMap] = React.useState(null)
 
-    const [selectedMarker, setSelectedMarker] = useState('');
-  
-    const onLoad = React.useCallback((map) => {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy! LOL
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-    
-    //https://developers.google.com/maps/documentation/javascript/maptypes#CustomMapTypes
+    const [selectedMarker, setSelectedMarker] = useState<WaypointMarker | null>(null);
 
-    var moonMapType = new google.maps.ImageMapType({
-    getTileUrl: function(coord, zoom) {
-        var normalizedCoord = getNormalizedCoord(coord, zoom);
-        if (!normalizedCoord) {
-            return null;
-        }
-        var bound = Math.pow(2, zoom);
-        return '//mw1.google.com/mw-planetary/lunar/lunarmaps_v1/clem_bw' +
-            '/' + zoom + '/' + normalizedCoord.x + '/' +
-            (bound - normalizedCoord.y - 1) + '.jpg';
-    },
-    tileSize: new google.maps.Size(256, 256),
-    maxZoom: 9,
-    minZoom: 0,
-    name: 'Moon'
-    });
-
-    map.mapTypes.set('moon', moonMapType);
-    map.setMapTypeId('moon');
-    
-  
-    setMap(map)
-    }, []);
-  
-    const onUnmount = React.useCallback(function callback(map) {
-      setMap(null)
-    }, [])
-  
     return isLoaded ? (
         <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={addMarsMapTypes}
+            options={{
+                streetViewControl: false,
+                mapTypeControlOptions: {
+                    mapTypeIds: ['elevation', 'visible', 'infrared']
+                }
+            }}
         >
-          {props.waypoints.map((marker) => {
-            return(
-                <div key={marker.waypoint_id}>
-                  <Marker 
-                    position = {new google.maps.LatLng(marker.location[0], marker.location[1])}
-                    onClick={()=>setSelectedMarker(marker)}
-                  />
-                </div>
-            );
-        })}
+            {props.waypoints.map((marker) => {
+                return (
+                    <div key={marker.waypoint_id}>
+                        <Marker
+                            position={new google.maps.LatLng(marker.location[0], marker.location[1])}
+                            onClick={() => setSelectedMarker(marker)}
+                        />
+                    </div>
+                );
+            })}
 
-        {selectedMarker  && (
-            <InfoWindow position = {new google.maps.LatLng(selectedMarker.location[0], selectedMarker.location[1])} 
-                        options = {{pixelOffset: new window.google.maps.Size(0,-40),}}>
-              <div>
-                <h1>Waypoint {selectedMarker.waypoint_id}</h1>
-                <p>Type: {selectedMarker.type}</p>
-                <p>Description: {selectedMarker.description}</p>
-                <p>Author: {selectedMarker.author}</p>
-              </div>
-            </InfoWindow>
-        )}
+            {selectedMarker && (
+                <InfoWindow position={new google.maps.LatLng(selectedMarker.location[0], selectedMarker.location[1])}
+                            options={{pixelOffset: new window.google.maps.Size(0, -40),}}
+                            onCloseClick={() => setSelectedMarker(null)}>
+                    <div>
+                        <h1>Waypoint {selectedMarker.waypoint_id}</h1>
+                        <p>Type: {selectedMarker.type}</p>
+                        <p>Description: {selectedMarker.description}</p>
+                        <p>Author: {selectedMarker.author}</p>
+                    </div>
+                </InfoWindow>
+            )}
 
         </GoogleMap>
     ) : <></>
-  }
+}
 
 export default WaypointMap;
-
-// waypoint_id: waypoint.waypoint_id,
-// location: waypoint.location,
-// type: waypoint.type,
-// description: waypoint.description,
-// author: waypoint.author
-
