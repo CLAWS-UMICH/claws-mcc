5b69c2146bd4b03b94cf74dcc0a2f8527613ee02
myanm <myanm@umich.edu>
2024-01-31 20:31:01 -0500

    add images

--- client/src/components/camera_view/CameraView.tsx
+++ client/src/components/camera_view/CameraView.tsx
@@ -1,7 +1,18 @@
 import React from "react"
-
+import CameraView1 from '../../assets/CameraView1';
+import CameraView2 from '../../assets/';
 
 // TODO: Implement
 const CameraView: React.FC = props => {
-    return null;
+    return(
+        <div className='camera-view-container'>
+            <div className='top-image'>
+                <img src={CameraView1} alt="camera"/>
+            </div>
+
+            <div className='bottom-image'>
+                <img src={CameraView2} alt="camera"/>
+            </div>
+        </div>
+    )
 }
