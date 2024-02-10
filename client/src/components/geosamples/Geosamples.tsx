a89cac9b5a4eabb9bc5ddac75b9a38f8bc329848
supermathtm <aprilyc@umich.edu>
2024-02-09 21:07:09 -0500

    adding map code

--- client/src/components/geosamples/Geosamples.tsx
+++ client/src/components/geosamples/Geosamples.tsx
@@ -54,21 +54,21 @@ const GeosampleManager: React.FC = () => {
     };
 
     return (
-        <SidebarMenu dropdownMenus={dropdownMenus}/>
-        // <div className={styles.root}>
-        // <InlineDrawer separator open>
-        //     <DrawerHeader>
-        //     <DrawerHeaderTitle>Samples</DrawerHeaderTitle>
-        //     </DrawerHeader>
-        //     <DrawerBody>
-        //         <SearchBox/>
-        //         <SamplesList/>
-        //     </DrawerBody>
-        // </InlineDrawer>
-        // <div className={styles.content}>
-        //     <p>This is the page content</p>
-        // </div>
-        // </div>
+        // <SidebarMenu dropdownMenus={dropdownMenus}/>
+        <div className={styles.root}>
+        <InlineDrawer separator open>
+            <DrawerHeader>
+            <DrawerHeaderTitle>Samples</DrawerHeaderTitle>
+            </DrawerHeader>
+            <DrawerBody>
+                <SearchBox/>
+                {/* <SamplesList/> */}
+            </DrawerBody>
+        </InlineDrawer>
+        <div className={styles.content}>
+            <p>This is the page content</p>
+        </div>
+        </div>
     );
 
     // return (
@@ -76,46 +76,45 @@ const GeosampleManager: React.FC = () => {
     //     // <SidebarMenu dropdownMenus={dropdownMenus} />
     // );
 };
-import React from 'react'
-import { WaypointMap, WaypointMapProps } from '../waypoints/WaypointMap.tsx';
-import {BaseWaypoint, ManagerAction, ManagerAction as MapAction, WaypointType} from "../waypoints/WaypointManager.tsx";
-import { WaypointList } from '../waypoints/WaypointList.tsx';
-import { Divider, InlineDrawer } from '@fluentui/react-components';
-import { WaypointsDrawer } from '../waypoints/WaypointsDrawer.tsx';
-import { waypointsReducer, ManagerState, WaypointManager, initialState } from '../waypoints/WaypointManager.tsx';
-import { useReducer, useState, useEffect } from 'react';
-import useWebSocket from 'react-use-websocket';
-import { ReadyState } from 'react-use-websocket';
+// import { WaypointMap } from '../waypoints/WaypointMap.tsx';
+// import {BaseWaypoint, ManagerAction, ManagerAction as MapAction, WaypointType} from "../waypoints/WaypointManager.tsx";
+// import { WaypointList } from '../waypoints/WaypointList.tsx';
+// // import { Divider, InlineDrawer } from '@fluentui/react-components';
+// import { WaypointsDrawer } from '../waypoints/WaypointsDrawer.tsx';
+// import { waypointsReducer, ManagerState, WaypointManager, initialState } from '../waypoints/WaypointManager.tsx';
+// import { useReducer, useState, useEffect } from 'react';
+// import useWebSocket from 'react-use-websocket';
+// import { ReadyState } from 'react-use-websocket';
 
-const GeosampleManager: React.FC = () => {
+// const GeosampleManager: React.FC = () => {
 
-    const [state, dispatch] = useReducer(waypointsReducer, initialState)
-    const [messageHistory, setMessageHistory] = useState<string[]>([]);
-    const {sendMessage, lastMessage, readyState} = useWebSocket("ws://localhost:8000/frontend", {
-        onOpen: () => sendMessage(JSON.stringify({type: 'GET_WAYPOINTS'}))
-    });
-    useEffect(() => {
-        if (lastMessage !== null) {
-            setMessageHistory((prev) => prev.concat(lastMessage.data));
-            dispatch({type: 'set', payload: JSON.parse(lastMessage.data).data});
-        }
-    }, [lastMessage, setMessageHistory]);
-    return (
-        <div style={{display: "flex"}}>
-            <InlineDrawer separator open>
-                <WaypointsDrawer selected={state.selected} waypoints={state.waypoints} dispatch={dispatch}
-                                 ready={readyState === ReadyState.OPEN}/>
-            </InlineDrawer>
-            <div className='waypoints-container'>
-                <WaypointList temp={state.temp} waypoints={state.waypoints} selected={state.selected}
-                              dispatch={dispatch}/>
-                <Divider/>
-                <WaypointMap waypoints={state.waypoints}
-                             selected={state.selected}
-                             dispatch={dispatch}/>
-            </div>
-        </div>
-    );
+//     const [state, dispatch] = useReducer(waypointsReducer, initialState)
+//     const [messageHistory, setMessageHistory] = useState<string[]>([]);
+//     const {sendMessage, lastMessage, readyState} = useWebSocket("ws://localhost:8000/frontend", {
+//         onOpen: () => sendMessage(JSON.stringify({type: 'GET_WAYPOINTS'}))
+//     });
+//     useEffect(() => {
+//         if (lastMessage !== null) {
+//             setMessageHistory((prev) => prev.concat(lastMessage.data));
+//             dispatch({type: 'set', payload: JSON.parse(lastMessage.data).data});
+//         }
+//     }, [lastMessage, setMessageHistory]);
+//     return (
+//         <div style={{display: "flex"}}>
+//             <InlineDrawer separator open>
+//                 <WaypointsDrawer selected={state.selected} waypoints={state.waypoints} dispatch={dispatch}
+//                                  ready={readyState === ReadyState.OPEN}/>
+//             </InlineDrawer>
+//             <div className='waypoints-container'>
+//                 <WaypointList temp={state.temp} waypoints={state.waypoints} selected={state.selected}
+//                               dispatch={dispatch}/>
+//                 <Divider/>
+//                 <WaypointMap waypoints={state.waypoints}
+//                              selected={state.selected}
+//                              dispatch={dispatch}/>
+//             </div>
+//         </div>
+//     );
 
     // OLD STUFF UNCOMMENT IF NEED
      // Define waypoints and other props as needed
@@ -150,6 +149,6 @@ const GeosampleManager: React.FC = () => {
         // </div>
         // x
     // )
-}
+// }
 
 export default GeosampleManager;
