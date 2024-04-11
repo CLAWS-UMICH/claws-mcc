767ed0dd0713b4746499a9112c6700205dcde2ab
Varun Sadasivam <sadavar05@gmail.com>
2024-04-10 20:01:12 -0400

    added messages component

---
+++ client/src/components/messages/Messages.tsx
@@ -0,0 +1,12 @@
+import React, {useEffect, useReducer, useState} from "react";
+import {Divider, InlineDrawer, DrawerHeader, Button, DrawerHeaderTitle} from "@fluentui/react-components";
+
+export const Messages: React.FC = () => {
+    return (
+        <div style={{display: "flex", height:"100vh", backgroundColor: "#000000"}}>
+            <h1>Messages</h1>
+        </div>
+    );
+}
+
+export default Messages;
