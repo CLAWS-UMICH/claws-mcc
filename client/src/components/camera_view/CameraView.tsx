4f4d951d9737aa245c5a2e2f60842e51a7fac468
Chipped1 <30380060+Chipped1@users.noreply.github.com>
2024-05-13 19:25:54 -0400

    add uptime (and ignore it when logging)

--- client/src/components/camera_view/CameraView.tsx
+++ client/src/components/camera_view/CameraView.tsx
@@ -1,27 +1,69 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import CameraView1 from '../../assets/cameraview1.jpeg';
 import CameraView2 from '../../assets/cameraview2.jpeg';
 import './cameraView.css';
+import useDynamicWebSocket from "../../hooks/useWebSocket";
 
-// TODO: Implement
-const CameraView: React.FC = props => {
+const CameraView: React.FC = () => {
+    const [uptime, setUpTime] = useState<number>(0);  
+
+    const {sendMessage, lastMessage} = useDynamicWebSocket({
+        onOpen: () => {
+            sendMessage(JSON.stringify({type: 'UPTIME'}));
+        }
+    });
+
+    useEffect(() => {
+        if (lastMessage) {
+            let uptime = 0;
+            try {
+                uptime = JSON.parse(lastMessage.data).data;
+            } catch (error) {
+                console.error(error);
+            }
+            setUpTime(uptime);
+        }
+    }, [lastMessage]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setUpTime(prevUptime => prevUptime + 1);
+        }, 1000);
+
+        const syncInterval = setInterval(() => {
+            sendMessage(JSON.stringify({type: 'UPTIME'}));
+        }, 5000);
+
+        return () => {
+            clearInterval(interval);
+            clearInterval(syncInterval);
+        };
+    }, [sendMessage]);
+
+    const formatUptime = (uptime: number) => {
+        const hours = Math.floor(uptime / 3600);
+        const minutes = Math.floor((uptime % 3600) / 60);
+        const seconds = uptime % 60;
+
+        return `${String(hours || 0).padStart(2, '0')}:${String(minutes || 0).padStart(2, '0')}:${String(seconds || 0).padStart(2, '0')}`;
+    };
+
     return(
         <div className='camera-view-container'>
             <div className='top-bar'>
                 <div className='blue-dot'></div>
-                01:05:21
+                {formatUptime(uptime)}
             </div>
             <div className="content camera-content">
                 <div className='top-image camera'>
                     <img src={CameraView1} alt='camera' className='image'/>
                 </div>
                 <div className='bottom-image camera'>
-                <img src={CameraView2} alt='camera' className='image'/>
+                    <img src={CameraView2} alt='camera' className='image'/>
                 </div>
             </div>
         </div>
-    )
+    );
 }
-
 
 export default CameraView;
